5032f7a79e5dc57cb3434390ee489e06138dc867
miticnikola <nikola92mitic@gmail.com>
2021-08-01 16:28:14 +0200

    adding active class in nav

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-let h1CurrentTime = document.getElementById('currentTime');
+// Active class for nav
+let aTagsNav = document.querySelectorAll('#navigation li a');
+console.log(aTagsNav);
+
+let deleteActive = () => {
+    aTagsNav.forEach(a => {
+        a.classList.remove("active");
+    });
+};
+
+aTagsNav.forEach(a => {
+    a.addEventListener('click', () => {
+        if (!a.classList.contains('active')) {
+            deleteActive();
+            a.classList.add('active');
+        }
+    });
+});
+
 
 // Current time
+let h1CurrentTime = document.getElementById('currentTime');
 let date = new Date();
 let hours = date.getHours();
 let minutes = date.getMinutes();
@@ -8,21 +27,21 @@ let seconds = date.getSeconds();
 h1CurrentTime.innerHTML = `${hours}: ${minutes}: ${seconds}`;
 
 // Updating time in one second
-setInterval(()=> {
+setInterval(() => {
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
-    if(hours < 10){
+    if (hours < 10) {
         hours = "0" + date.getHours();
     }
-    
-    if(minutes < 10){
+
+    if (minutes < 10) {
         minutes = "0" + date.getMinutes();
     }
-    
-    if(seconds < 10){
+
+    if (seconds < 10) {
         seconds = "0" + date.getSeconds();
     }
 
@@ -41,23 +60,23 @@ let counter = 0;
 h3StopwatchResult.innerHTML = `0`;
 
 btnStart.addEventListener('click', () => {
-    if(clock == null){
-      clock = setInterval(() => {
+    if (clock == null) {
+        clock = setInterval(() => {
             counter++;
 
             h3StopwatchResult.innerHTML = `${counter}s`;
 
-            if(counter > 59){
+            if (counter > 59) {
                 let seconds = counter % 60;
-                let minutes = (counter - seconds) / 60;  
+                let minutes = (counter - seconds) / 60;
                 h3StopwatchResult.innerHTML = `${minutes}m : ${seconds + 1}s`;
-     
-                if(minutes > 59){
-                    let minutes1 = minutes % 60; 
+
+                if (minutes > 59) {
+                    let minutes1 = minutes % 60;
                     let hours = (minutes - (minutes1 % 60)) / 60;
                     h3StopwatchResult.innerHTML = `${hours}h : ${minutes1}m : ${seconds}s`;
                 }
-                
+
             }
 
 
@@ -66,7 +85,7 @@ btnStart.addEventListener('click', () => {
 });
 
 btnPause.addEventListener('click', () => {
-    if(btnReset.clicked == true){
+    if (btnReset.clicked == true) {
         counter = 0;
     } else {
         clearInterval(clock);
